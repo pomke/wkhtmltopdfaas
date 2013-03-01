39903595f4a6a6ad324073cb70c064f9d8c2d2c7
Pomke <pomke@pomke.com>
2013-03-01 14:24:27 +1100

    not working

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var spawn = require('child_process').spawn;
 var restify = require('restify');
 
 var serverOptions = {
-  name: 'wkhtmltopfd-aas',
-  version: '1.0.0'
+    name: 'wkhtmltopfd-aas',
+    version: '1.0.0'
 };
 
 var server = restify.createServer(serverOptions);
@@ -14,24 +14,30 @@ server.use(restify.queryParser());
 // server.use(restify.bodyParser());
 
 server.get('/', function(req, res, next) {
-  if (req.params.url === undefined)
-    return next(new restify.MissingParameterError('url'));
-  
-  res.header('Content-Type', 'application/pdf');
-  res.header('Content-Disposition', 'attachment; filename="print.pdf"');
+    if (req.params.url === undefined) {
+        return next(new restify.MissingParameterError('url'));
+    }
+    var filename=req.params.filename || 'print.pdf';
+    res.header('Content-Type', 'application/pdf');
+    res.header('Content-Disposition', 'attachment; filename="'+filename+'"');
 
-  var cmd = 'wkhtmltopdf';
-  var args = ['-q', req.params.url, '-'];
-
-  var child = spawn(cmd, args);
-  child.stdout.pipe(res, {end: false});
-
-  child.on('exit', function(code) {
-    if (code !== 0)
-      return next(new restify.InternalError('wkhtmltopdf failed'));
-
-    return res.end();
-  });
+    var cmd = 'wkhtmltopdf';
+    var args = ['-q', req.params.url, '-'];
+    console.log(cmd, args.join(' '));
+    var child = spawn(cmd, args);
+    child.stdout.pipe(res, {end: false});
+    child.on("error", function(e) {
+        console.log("ERR", e);  
+    });
+    child.on('exit', function(code) {
+        if (code !== 0) {
+            console.log('non zero');
+            return next(new restify.InternalError('wkhtmltopdf failed'));
+        } else { 
+            console.log('success!');
+            return res.end();
+        }
+    });
 });
 
 server.listen(8000);
